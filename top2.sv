a53d41e53bb89a72a50a4d5fbb2cea462d1b5a7b
adinavale <68146367+adinavale@users.noreply.github.com>
2022-10-22 13:30:29 -0700

    top update

--- top2.sv
+++ top2.sv
@@ -1,7 +1,6 @@
 // a simple top level file for vid5
 `timescale 1ns/10ps
 
-`include "vid5b.sv"
 `include "intf.svh"
 
 package mejuvm;
@@ -66,4 +65,4 @@ module top();
 
 endmodule : top
 
-
+`include "vid5b.sv"
